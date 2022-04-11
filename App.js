56d66cf5317c3e6985bd4ff2b65fffd43f85ef47
Bhakti Daraniya <180540107035@darshan.ac.in>
2022-04-11 10:01:32 +0530

    second

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World!</Text>
+      <Text>Welcome to React Native.</Text>
       <StatusBar style="auto" />
     </View>
   );
